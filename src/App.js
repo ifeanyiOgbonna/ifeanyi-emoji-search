959619e0139761cf17610bd24c8bf20580e96334
Ifeanyi Ogbonna <ogbonna.ifybest@gmail.com>
2023-02-25 21:31:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import emojione from "emojione";
+import EmojiData from "./Emoji.json";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const newData = EmojiData.filter((emoji) =>
+      emoji.name.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+    setData(newData);
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Emoji Search</h1>
+        <input
+          type="text"
+          name="search"
+          placeholder="Enter Anything"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </center>
+      {data.map((emoji, index) => (
+        <div key={index}>
+          <div className="card">
+            <div className="card-detail">
+            <span
+            dangerouslySetInnerHTML={{
+              __html: emojione.shortnameToImage(emoji.shortname),
+            }}
+          />
+          <span>{emoji.name}</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
